59e66135c9f955dc228bbab5ccc3b1a137d121f7
veranoo <przemyslaw.afeltowicz@gmail.com>
2016-11-15 17:29:24 +0100

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,12 @@ $(window).on('load', function () { // makes sure the whole site is loaded
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(100).css({'overflow': 'visible'});
 })
+
+new WOW().init();
+
+$(function(){
+    $(".main-title").typed({
+        strings: ["^1000 Front-end ^1000 developer."],
+        typeSpeed: 20
+    });
+});
